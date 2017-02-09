7b06800062a775a9e9ed29eddddabc265c556ae2
Kevin Jiah-Chih Liao <me@liaokev.in>
2017-02-09 15:17:21 -0500

    Refactor form into separate component.

--- src/Components/form.jsx
+++ src/Components/form.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Firebase from '../Firebase';
+import SpeechForm from '../Components/speechform';
 
 const database = Firebase.database();
 
@@ -13,37 +14,11 @@ class Form extends Component {
       content: "",
       error: ""
     };
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   handleChange(event) {
     const name = event.target.name;
     this.setState({[name]: event.target.value});
-  }
-
-  handleSubmit(event) {
-    event.preventDefault();
-    const newSpeech = {
-      speaker: this.state.speaker,
-      classYear: this.state.classYear,
-      email: this.state.email,
-      content: this.state.content
-    }
-    console.log("Creating a new speech: ", newSpeech);
-    const newSpeechKey = database.ref().child('speeches').push().key;
-    const updates = { ['/speeches/' + newSpeechKey]: newSpeech };
-
-    database.ref().update(updates);
-    this.setState({
-      speaker: "",
-      classYear: "",
-      email: "",
-      content: ""
-    });
-    /* this.resetForm(() => {
-     *   console.log("State is now: ", this.state);
-     * }).bind(this);*/
   }
 
   resetForm(callback) {
@@ -76,30 +51,7 @@ class Form extends Component {
             </ul>
           </div>
         </div>
-        <form onSubmit={this.handleSubmit} >
-          <label>
-            Name:
-            <input onChange={this.handleChange} type="text" name="speaker" />
-          </label>
-          <label>
-            Class Year:
-            <input onChange={this.handleChange} type="text" name="classYear" />
-          </label>
-          <label>
-            Email:
-            <input onChange={this.handleChange} type="text" name="email" />
-          </label>
-          <label>
-            Content:
-            <textarea
-                onChange={this.handleChange}
-                type="text"
-                name="content"
-                value={this.state.value}
-            />
-          </label>
-          <input type="submit" value="Submit" />
-        </form>
+        <SpeechForm />
       </section>
 
     );
---
+++ src/Components/speechform.jsx
@@ -0,0 +1,78 @@
+import React, { Component } from 'react';
+import Firebase from '../Firebase';
+
+const database = Firebase.database();
+
+class SpeechForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      speaker: "",
+      classYear: "",
+      email: "",
+      content: "",
+      error: ""
+    }
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    const name = event.target.name;
+    this.setState({[name]: event.target.value});
+  }
+  
+  handleSubmit(event) {
+    event.preventDefault();
+    const newSpeech = {
+      speaker: this.state.speaker,
+      classYear: this.state.classYear,
+      email: this.state.email,
+      content: this.state.content
+    }
+    console.log("Creating a new speech: ", newSpeech);
+    const newSpeechKey = database.ref().child('speeches').push().key;
+    const updates = { ['/speeches/' + newSpeechKey]: newSpeech };
+
+    database.ref().update(updates);
+    this.setState({
+      speaker: "",
+      classYear: "",
+      email: "",
+      content: ""
+    });
+    /* this.resetForm(() => {
+     *   console.log("State is now: ", this.state);
+     * }).bind(this);*/
+  }
+  render () {
+    return (
+      <form onSubmit={this.handleSubmit} >
+        <label>
+          Name:
+          <input onChange={this.handleChange} type="text" name="speaker" />
+        </label>
+        <label>
+          Class Year:
+          <input onChange={this.handleChange} type="text" name="classYear" />
+        </label>
+        <label>
+          Email:
+          <input onChange={this.handleChange} type="text" name="email" />
+        </label>
+        <label>
+          Content:
+          <textarea
+              onChange={this.handleChange}
+              type="text"
+              name="content"
+              value={this.state.value}
+          />
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
+    );
+  }
+}
+
+export default SpeechForm;
