1ecafbf9941ddabd485501635dc16eb92bf73749
Kevin Jiah-Chih Liao <me@liaokev.in>
2017-02-19 16:54:53 -0500

    Plenary is not in session.

--- src/Components/quorum.jsx
+++ src/Components/quorum.jsx
@@ -4,8 +4,8 @@ class Quorum extends Component {
   render() {
     return (
       <section className="quorumCounter" >
-        <h3>We are currently at Quorum.</h3>
-        <p>It is currently constitutional for plenary to be in session.</p>
+        <h3>We are currently NOT at Quorum.</h3>
+        <p>It is currently NOT constitutional for plenary to be in session.</p>
         <p>Today, the number of students required to reach and maintain quorum is:</p>
         <h3>700</h3>
       </section>
